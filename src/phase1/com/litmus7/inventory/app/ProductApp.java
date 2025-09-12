3d1ebb27a68ab4a127dc84bbdbcd1c157943ad2a
Sruthy K S <sruthysuniloc@gmail.com>
2025-09-12 22:43:22 +0530

    created controller

--- src/phase1/com/litmus7/inventory/app/ProductApp.java
+++ src/phase1/com/litmus7/inventory/app/ProductApp.java
@@ -1,7 +1,5 @@
 package phase1.com.litmus7.inventory.app;
 
-
-
 public class ProductApp {
 	public static void main(String[] args) {
         
---
+++ src/phase1/com/litmus7/inventory/controller/ProductController.java
@@ -0,0 +1,5 @@
+package phase1.com.litmus7.inventory.controller;
+
+public class ProductController {
+
+}

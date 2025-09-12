0475dc9589d7cbf192bec2c6af2362354f2afdb1
Sruthy K S <sruthysuniloc@gmail.com>
2025-09-12 23:17:58 +0530

    updated

--- src/phase1/com/litmus7/inventory/controller/ProductController.java
+++ src/phase1/com/litmus7/inventory/controller/ProductController.java
@@ -32,58 +32,7 @@ public class ProductController {
         
     }
 
-    private void processFile(File file) {
-        logger.info("Processing file: " + file.getName());
-
-        boolean success = false; 
-
-        try (Connection conn = DBConnectionUtil.getConnection()) {
-            conn.setAutoCommit(false);
-
-            try (
-                BufferedReader br = new BufferedReader(new FileReader(file));
-                PreparedStatement ps = conn.prepareStatement(
-                    "INSERT INTO products (sku, product_name, quantity, price) VALUES (?, ?, ?, ?)")
-            ) {
-                String line = br.readLine(); //header
-
-                while ((line = br.readLine()) != null) {
-                    String[] parts = line.split(",");
-                    if (parts.length < 4) {
-                        throw new SQLException("Invalid record format: " + line);
-                    }
-
-                    ps.setInt(1, Integer.parseInt(parts[0].trim()));
-                    ps.setString(2, parts[1].trim());
-                    ps.setInt(3, Integer.parseInt(parts[2].trim()));
-                    ps.setDouble(4, Double.parseDouble(parts[3].trim()));
-                    ps.executeUpdate();
-                }
-
-                conn.commit();
-                success = true;
-                logger.info("File committed: " + file.getName());
-
-            } catch (Exception e) {
-                try {
-                    conn.rollback();
-                } catch (SQLException sqle) {
-                    logger.error("Rollback failed for " + file.getName() + ": " + sqle.getMessage());
-                }
-                logger.error("Error in file " + file.getName() + " → rollback. Reason: " + e.getMessage());
-            }
-
-        } catch (Exception ex) {
-            logger.error("DB connection error for file " + file.getName() + ": " + ex.getMessage());
-        }
-
-
-        if (success) {
-            moveFile(file, PROCESSED_DIR);
-        } else {
-            moveFile(file, ERROR_DIR);
-        }
-    }
+    
 
 
     private void moveFile(File file, String targetDir) {

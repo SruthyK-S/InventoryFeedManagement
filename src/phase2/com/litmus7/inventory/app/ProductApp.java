79fccf29d63642e6765b2f38de3570b567e77520
Sruthy K S <sruthysuniloc@gmail.com>
2025-09-13 21:08:43 +0530

    updated

---
+++ src/phase2/com/litmus7/inventory/app/ProductApp.java
@@ -0,0 +1,5 @@
+package phase2.com.litmus7.inventory.app;
+
+public class ProductApp {
+
+}
---
+++ src/phase2/com/litmus7/inventory/controller/ProductController.java
@@ -0,0 +1,5 @@
+package phase2.com.litmus7.inventory.controller;
+
+public class ProductController {
+
+}
